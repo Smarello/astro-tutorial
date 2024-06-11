437671b40b3f7a4d81e175b3c6cb885fd9c25813
Stefano Marello <smarello89@gmail.com>
2024-06-11 22:17:52 +0200

    Add exernal typescript

---
+++ src/scripts/menu.ts
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger');
+
+if (hamburger) {
+  hamburger.addEventListener('click', () => {
+    const navLinks = document.querySelector('.nav-links');
+    if (navLinks) navLinks.classList.toggle('expanded');
+  });
+}
